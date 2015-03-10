85dde763256a7062e2d2da014f299695952cfe87
jeffersd <jeffersd@email.arizona.edu>
2015-03-10 02:33:08 -0700

    Have gui version of program in basic working form. Need to streamline OO operation and cohesiveness, change button event so new panel is seen quicker, make new panel appear properly, fix output messages, make crawler return status as program progresses, make gui look prettier (presentation and color), watch password field deprecation, more tests.

--- src/view/ViewController.java
+++ src/view/ViewController.java
@@ -1,14 +1,15 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -25,13 +26,20 @@ public class ViewController extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private int viewWidth = 500;
-	private int viewHeight = 300;
-	private int startLocationX = 100;
-	private int startLocationY = 100;
-	//private WebCrawler model;
+	private JPanel startPanel;
+	private JPanel mainPanel;
+	private JTextField usernameField;
+	private JPasswordField passwordField;
+	
+	private int viewWidth = 300;
+	private int viewHeight = 100;
+	private int startLocationX = 150;
+	private int startLocationY = 150;
+	private WebCrawler model;
 
 	public ViewController() {
+		model = new WebCrawler();
+		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setSize(viewWidth, viewHeight);
@@ -39,42 +47,82 @@ public class ViewController extends JFrame {
 		frame.setLocation(startLocationX, startLocationY);
 		frame.setLayout(new BorderLayout());
 		frame.setResizable(false);
-		
-		//model = new WebCrawler();
-		
 		frame.setTitle("Coupon Adder");
 		
+		startPanel = addPanel(3, 1);
+		mainPanel = addPanel(1, 2);
+		
+		addTextArea(mainPanel, "Status Field");
+		addTextArea(mainPanel, "Coupons");
+		
+		addLabel(startPanel, "Username: ");
+		usernameField = addTextField(startPanel, "");
+		addLabel(startPanel, "Password: ");
+		passwordField = new JPasswordField();
+		startPanel.add(passwordField);
+		passwordField.setVisible(true);
+		
 		JButton addCouponsButton = new JButton("Add Coupons");
-		frame.add(addCouponsButton, BorderLayout.PAGE_END);
+		AddCouponsButtonActionListener AL = new AddCouponsButtonActionListener();
+		addCouponsButton.addActionListener(AL);
+		//frame.add(addCouponsButton, BorderLayout.PAGE_END);
+		startPanel.add(addCouponsButton);
 		
-//		JTextArea statusField = new JTextArea("Status Field");
-//		statusField.setEditable(false);
-//		frame.add(statusField, BorderLayout.PAGE_START);
-//		statusField.setVisible(true);
-		addTextArea(frame, "Status Field", BorderLayout.PAGE_START);
-		
-//		JTextArea couponsAddedField = new JTextArea("Coupons");
-//		couponsAddedField.setEditable(false);
-//		frame.add(couponsAddedField, BorderLayout.WEST);
-//		couponsAddedField.setVisible(true);
-		addTextArea(frame, "Coupons", BorderLayout.CENTER);
-		addTextField(frame, "Username", BorderLayout.EAST);
-		addTextField(frame, "Password", BorderLayout.EAST);
-		
+		frame.add(mainPanel);
+		frame.add(startPanel);
+		startPanel.setVisible(true);
 		frame.setVisible(true);
 	}
 	
-	public void addTextArea(JFrame frame, String name, String location) {
+	public JPanel addPanel(int rows, int cols) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(rows, cols));
+		panel.setVisible(false);
+		return panel;
+	}
+	
+	public void addTextArea(JPanel panel, String name) {
 		JTextArea field = new JTextArea(name);
 		field.setEditable(false);
-		frame.add(field, location);
+		panel.add(field);
 		field.setVisible(true);
 	}
 	
-	public void addTextField(JFrame frame, String name, String location) {
+	public JTextField addTextField(JPanel panel, String name) {
 		JTextField field = new JTextField(name);
 		field.setEditable(true);
-		frame.add(field, location);
+		panel.add(field);
 		field.setVisible(true);
+		return field;
 	}
+	
+	public void addLabel(JPanel panel, String labelText) {
+		JLabel label = new JLabel(labelText);
+		panel.add(label);
+	}
+	
+	private class AddCouponsButtonActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				startPanel.setVisible(false);
+				mainPanel.setVisible(true);
+				int coupons = 0;
+				model.setUsername(usernameField.getText());
+				//char[] passwordAsCharArray = passwordField.getPassword();
+				model.setPassword(passwordField.getText());
+				coupons = model.addAllCoupons();
+				if (coupons == -1) {
+					startPanel.setVisible(true);
+					mainPanel.setVisible(false);
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
 }
