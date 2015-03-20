21ed4f58439e8cba89c96bbe3453a8ebd6a93e71
jeffersd <jeffersd@email.com>
2015-03-20 15:56:59 -0700

    added tests and came upon error in add all coupons test

--- src/view/ViewController.java
+++ src/view/ViewController.java
@@ -18,7 +18,9 @@ import javax.swing.JTextField;
 import model.WebCrawler;
 
 /**
- * Controller class for the graphical view of this program. Contains the main driver methods for the app
+ * Controller class for the graphical view of this program. Contains the main
+ * driver methods for the app
+ * 
  * @author dillon
  *
  */
@@ -27,73 +29,79 @@ public class ViewController extends JFrame implements Observer {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel startPanel;
 	private JPanel mainPanel;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
 	private JTextArea statusArea;
-	
+
 	private int viewWidth = 300;
 	private int viewHeight = 110;
 	private int startLocationX = 150;
 	private int startLocationY = 150;
 	public WebCrawler model;
 
+	/**
+	 * Constructor for the GUI class
+	 * 
+	 * @param makeGUI
+	 *            True if we want the visual component constructed, false if we
+	 *            want to test.
+	 */
 	public ViewController() {
 		model = new WebCrawler(this); // send down self as observer
-		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setSize(viewWidth, viewHeight);
 		frame.pack();
 		frame.setLocation(startLocationX, startLocationY);
 		frame.setLayout(new BorderLayout());
-		//frame.setResizable(false);
+		// frame.setResizable(false);
 		frame.setTitle("Coupon Adder");
-		
+
 		statusArea = new JTextArea("Status");
 		frame.add(statusArea, BorderLayout.PAGE_START);
-		
+
 		startPanel = addPanel(3, 1);
 		mainPanel = addPanel(1, 2);
-		
-		//addTextArea(mainPanel, "Status Field");
+
+		// addTextArea(mainPanel, "Status Field");
 		addTextArea(mainPanel, "Coupons");
-		
+
 		addLabel(startPanel, "Username: ");
 		usernameField = addTextField(startPanel, "");
 		addLabel(startPanel, "Password: ");
 		passwordField = new JPasswordField();
 		startPanel.add(passwordField);
 		passwordField.setVisible(true);
-		
+
 		JButton addCouponsButton = new JButton("Add Coupons");
 		AddCouponsButtonActionListener AL = new AddCouponsButtonActionListener();
 		addCouponsButton.addActionListener(AL);
-		//frame.add(addCouponsButton, BorderLayout.PAGE_END);
+		// frame.add(addCouponsButton, BorderLayout.PAGE_END);
 		startPanel.add(addCouponsButton);
-		
+
 		frame.add(mainPanel);
 		frame.add(startPanel);
 		startPanel.setVisible(true);
 		frame.setVisible(true);
 	}
-	
+
 	public JPanel addPanel(int rows, int cols) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(rows, cols));
 		panel.setVisible(false);
 		return panel;
 	}
-	
+
 	public void addTextArea(JPanel panel, String name) {
 		JTextArea field = new JTextArea(name);
 		field.setEditable(false);
 		panel.add(field);
 		field.setVisible(true);
 	}
-	
+
 	public JTextField addTextField(JPanel panel, String name) {
 		JTextField field = new JTextField(name);
 		field.setEditable(true);
@@ -101,7 +109,7 @@ public class ViewController extends JFrame implements Observer {
 		field.setVisible(true);
 		return field;
 	}
-	
+
 	public void addLabel(JPanel panel, String labelText) {
 		JLabel label = new JLabel(labelText);
 		panel.add(label);
@@ -185,12 +193,12 @@ public class ViewController extends JFrame implements Observer {
 				e1.printStackTrace();
 			}
 		}
-		
+
 	}
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		statusArea.setText((String) arg1);
 	}
-	
+
 }
